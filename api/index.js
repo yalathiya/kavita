8b9d3f630e77a92eccc9c62211ab451e02d21040
Yash Lathiya <yashlathiya59ppsv@gmail.com>
2023-04-24 23:22:48 +0530

    add controllers & routes

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 import express from "express";
-
+import postRoutes from "./routes/posts.js";
 const app = express();
 
 app.use(express.json());
+app.use("/api/posts", postRoutes);
 
 app.listen(8000, () => {
   console.log("Connected");
