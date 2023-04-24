cbfea434d8ca629871303912644dcb4e520b97c6
Yash Lathiya <yashlathiya59ppsv@gmail.com>
2023-04-24 12:24:27 +0530

    Home page is updated & post is updated

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-const Home = () => {
+const Menu = () => {
   const posts = [
     {
       id: 1,
@@ -35,25 +34,17 @@ const Home = () => {
     },
   ];
   return (
-    <div className="home">
-      <div className="posts">
-        {posts.map((post) => (
-          <div className="post" key={post.id}>
-            <div className="img">
-              <img src={post.img} />
-            </div>
-            <div className="content">
-              <Link className="link" to={`/post/${post.id}`}>
-                <h1>{post.title}</h1>
-              </Link>
-              <p>{post.desc}</p>
-              <button>Read</button>
-            </div>
-          </div>
-        ))}
-      </div>
+    <div className="menu">
+      <h1>Other creations you may like</h1>
+      {posts.map((post) => (
+        <div className="post" key={post.id}>
+          <img src={post.img} alt="" />
+          <h2>{post.title}</h2>
+          <button>Read More</button>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Home;
+export default Menu;
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img src="https://cdn-icons-png.flaticon.com/512/1088/1088537.png" />
+        <div className="user">
+          <img src="https://cdn-icons-png.flaticon.com/512/1088/1088537.png" />
+          <div className="info">
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+        <h1>LOrem500</h1>
+        <p>
+          <p>lorem001</p>
+          <br />
+          <p>lorem 0025</p>
+        </p>
+      </div>
+      <Menu />
+    </div>
+  );
 };
 
 export default Single;
